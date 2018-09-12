c6e3a5e5f87b2a775e58dc352eb9ab0333c8a473
Brennan D Baraban <375@holbertonschool.com>
2018-09-12 20:28:54 +0000

    Completed task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * File: 4-puts.c
+ * Desc: Prints exactly "Programming is like building
+ *       a multilingual puzzle", followed by a new line.
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
