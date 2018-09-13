a00438c50684b86e4fda642d74dacf62f5b1f261
Brennan D Baraban <375@holbertonschool.com>
2018-09-13 23:11:18 +0000

    Completed bonus task 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: 0 if successful, non-zero otherwise.
+ */
+int main(void)
+{
+	int digit1 = 0;
+	int digit2, digit3, digit4;
+
+	while (digit1 < 10)
+	{
+		for (digit2 = 0; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit1; digit3 < 10; digit3++)
+			{
+				for (digit4 = digit2 + 1; digit4 < 10; digit4++)
+				{
+					putchar((digit1 % 10) + '0');
+					putchar((digit2 % 10) + '0');
+					putchar(32);
+					putchar((digit3 % 10) + '0');
+					putchar((digit4 % 10) + '0');
+
+					if (digit1 == 9 && digit2 == 8 &&
+					    digit3 == 9 && digit4 == 9)
+					{
+						break;
+					}
+					else
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
