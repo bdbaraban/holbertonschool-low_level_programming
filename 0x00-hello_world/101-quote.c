a2845eee0fb8b10af385973c495fb05ed5d2b6bb
Brennan D Baraban <375@holbertonschool.com>
2018-09-13 20:02:42 +0000

    Completed task 0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * Auth: Brennan D Baraban
  */
 
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -13,7 +13,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - "
+	write(2, "and that piece of art is useful\" - "
 		"Dora Korpar, 2015-10-19\n");
 
 	return (1);
