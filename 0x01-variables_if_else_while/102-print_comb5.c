bd4704e87f0e5b6fbd1eb52e190324552eca3e09
Brennan D Baraban <375@holbertonschool.com>
2018-09-13 23:43:12 +0000

    Fixed error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,29 @@
  */
 int main(void)
 {
-	int digit1 = 0;
-	int digit2, digit3, digit4;
+	int digit1 = 0, digit2, digit3, digit4;
 
 	while (digit1 < 10)
 	{
-		for (digit2 = 0; digit2 < 9; digit2++)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
 			for (digit3 = digit1; digit3 < 10; digit3++)
 			{
-				for (digit4 = digit2 + 1; digit4 < 10; digit4++)
+				for (digit4 = 0; digit4 < 10; digit4++)
 				{
-					putchar((digit1 % 10) + '0');
-					putchar((digit2 % 10) + '0');
-					putchar(32);
-					putchar((digit3 % 10) + '0');
-					putchar((digit4 % 10) + '0');
-
+					if (digit3 == digit1 &&
+					    digit4 <= digit2)
+					{
+						continue;
+					}
+					else
+					{
+						putchar((digit1 % 10) + '0');
+						putchar((digit2 % 10) + '0');
+						putchar(32);
+						putchar((digit3 % 10) + '0');
+						putchar((digit4 % 10) + '0');
+					}
 					if (digit1 == 9 && digit2 == 8 &&
 					    digit3 == 9 && digit4 == 9)
 					{
@@ -46,6 +52,5 @@ int main(void)
 		digit1++;
 	}
 	putchar('\n');
-
 	return (0);
 }
