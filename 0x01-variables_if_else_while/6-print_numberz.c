7f36b06c9eb371b2d4a00c8431a38cde4e20bd17
Brennan D Baraban <375@holbertonschool.com>
2018-09-13 21:33:02 +0000

    Completed task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: 0 if successful, non-zero otherwise.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
