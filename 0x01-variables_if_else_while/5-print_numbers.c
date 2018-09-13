ed827078c98e44eb2bd48fff72ea5c14c1a10689
Brennan D Baraban <375@holbertonschool.com>
2018-09-13 21:13:14 +0000

    Completed task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0 if successful, non-zero otherwise.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
