fb4f75610151097f3777a4de683379d2390f6013
Brennan D Baraban <375@holbertonschool.com>
2018-09-14 17:35:29 +0000

    Fixed error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,8 +13,9 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - "
-		"Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
