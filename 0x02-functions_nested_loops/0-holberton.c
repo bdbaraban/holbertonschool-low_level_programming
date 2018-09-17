625701e6a88023d5b037fe9852bcbf94dcdcc0ab
Brennan D Baraban <375@holbertonschool.com>
2018-09-17 18:21:42 +0000

    Completed task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include "holberton.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: 0 is successful, non-zero otherwise.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
