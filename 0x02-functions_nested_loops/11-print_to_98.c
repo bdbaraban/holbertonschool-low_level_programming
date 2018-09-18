25c40ee3bbc1bcbd7d6542453cf4ce97023458be
Brennan D Baraban <375@holbertonschool.com>
2018-09-18 21:40:44 +0000

    Fixed styling

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,6 @@
  * print_to_98 - Prints all natural numbers from input value to 98,
  *               in order, separated by a comma followed by a space.
  * @n: The number to begin counting at.
- *
  */
 void print_to_98(int n)
 {
@@ -21,7 +20,6 @@ void print_to_98(int n)
 				_putchar((n / 100) + '0');
 			_putchar(((n / 10) % 10) + '0');
 			_putchar((n % 10) + '0');
-			
 			if (n == 98)
 				break;
 			_putchar(',');
@@ -46,7 +44,6 @@ void print_to_98(int n)
 					_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
-
 			if (n == 98)
 				break;
 			_putchar(',');
