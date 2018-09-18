7c5b867ee07cb9f62837ea666dbe87d1ddd52d1a
Brennan D Baraban <375@holbertonschool.com>
2018-09-18 20:08:36 +0000

    Completed task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
