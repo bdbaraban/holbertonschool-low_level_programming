50682abdfdccfe539ed82fa55634b917b9d96784
Brennan D Baraban <375@holbertonschool.com>
2018-09-18 20:45:19 +0000

    Fixed error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,14 +13,9 @@
  */
 int print_last_digit(int n)
 {
-	if (n <= 0)
-	{
-		_putchar((-n % 10) + '0');
-		return (-n % 10);
-	}
-	else
-	{
-		_putchar((n % 10) + '0');
-		return (n % 10);
-	}
+	if (n < 0)
+		n = _abs(n);
+
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
