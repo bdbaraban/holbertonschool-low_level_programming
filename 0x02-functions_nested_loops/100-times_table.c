40d0c7066d8192882b16b7d6a8b985131f3785e1
Brennan D Baraban <375@holbertonschool.com>
2018-09-18 22:45:24 +0000

    Fixed syntax error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,7 +34,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod / 10) % 10 + '0');
 				}
 				else if (prod < 100 && prod >= 10)
 				{
@@ -50,8 +50,6 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	else
-		return;
 }
 
 
