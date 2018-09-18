6e834630e7188614be710fd8af430b85fae48e9c
Brennan D Baraban <375@holbertonschool.com>
2018-09-18 21:12:46 +0000

    Fixed error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,21 +17,20 @@ void times_table(void)
 		for (mult = 0; mult <= 9; mult++)
 		{
 			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
 
 			if (prod >= 10)
 				_putchar((prod / 10) + '0');
-				
+
 			_putchar((prod % 10) + '0');
 
 			if (mult == 9)
 				continue;
 
 			_putchar(',');
-			_putchar(' ');	
-			if (prod <= 9)
-				_putchar(' ');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
 }
-
