44d4234d880e000d8015763b240d9f4425809d85
Brennan D Baraban <375@holbertonschool.com>
2018-09-19 03:07:43 +0000

    Fixed styling

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,6 @@ void print_times_table(int n)
 		_putchar('0');
 		_putchar('\n');
 	}
-
 	else if (n > 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
@@ -27,11 +26,9 @@ void print_times_table(int n)
 			_putchar('0');
 			_putchar(',');
 			_putchar(' ');
-
 			for (mult = 1; mult <= n; mult++)
 			{
 				prod = num * mult;
-
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
@@ -47,10 +44,8 @@ void print_times_table(int n)
 					_putchar((prod / 10) + '0');
 				}
 				_putchar((prod % 10) + '0');
-
 				if (mult == n)
 					continue;
-
 				_putchar(',');
 				_putchar(' ');
 			}
