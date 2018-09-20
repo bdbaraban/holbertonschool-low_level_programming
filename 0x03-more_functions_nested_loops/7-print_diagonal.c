2b99371385d5250f93be5e4aee5a47a6ebbd2bab
Brennan D Baraban <375@holbertonschool.com>
2018-09-20 21:19:20 +0000

    Fixed spacing

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 	{
 		for (len = 0; len < n; len++)
 		{
-			for (space = 0; space <= len; space++)
+			for (space = 0; space < len; space++)
 				_putchar(' ');
 			_putchar('\\');
 
