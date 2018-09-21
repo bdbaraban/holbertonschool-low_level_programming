be8b9c341544484c50469f859e4cdf167964b0e8
Brennan D Baraban <375@holbertonschool.com>
2018-09-21 01:10:56 +0000

    Completed bonus task 12

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * shift - Prints an integer one digit at a time.
+ * @num: The integer to be printed.
+ */
+void shift(int num)
+{
+	if ((num / 10) > 0)
+		shift(num / 10);
+	
+	_putchar((num % 10) + '0');	
+}
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	shift(n);
+}
