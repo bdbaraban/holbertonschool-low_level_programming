a02040a0da7deb0fed6057a6676cd37a6a618a7a
Brennan D Baraban <375@holbertonschool.com>
2018-09-21 04:29:28 +0000

    Fixed type conversion

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,20 @@
  */
 void print_number(int n)
 {
+	unsigned num;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -(unsigned)n;
+		num = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	if ((n / 10) > 0)
-		print_number(n / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
