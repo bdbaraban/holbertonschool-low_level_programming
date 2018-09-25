3aca7910e2be0a8e247bbf47e19b09d413f8096c
Brennan D Baraban <375@holbertonschool.com>
2018-09-25 19:49:41 +0000

    Fixed neg/pos checking

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -18,10 +18,7 @@ int _atoi(char *s)
 
 	do {
 		if (*s == '-')
-			sign = -1;
-
-		else if (*s == '+' || *s == ' ')
-			sign = 1;
+			sign *= -1;
 
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
