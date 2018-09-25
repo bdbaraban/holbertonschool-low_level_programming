0a09a610a70d54be9a2ce5f1e8ace81eb936e430
Brennan D Baraban <375@holbertonschool.com>
2018-09-25 19:30:05 +0000

    Switched to for loop

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,13 @@
  */
 void puts2(char *str)
 {
-	while (*str && *str != '\0')
-	{
-		_putchar(*str);
-		str += 2;
-	}
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
 
 	_putchar('\n');
 }
