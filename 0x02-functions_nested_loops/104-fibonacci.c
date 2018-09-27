60d24c5d072d965ae74dbb533dc753e4d2e80323
Brennan D Baraban <375@holbertonschool.com>
2018-09-27 03:52:09 +0000

    Cleaned code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -36,12 +36,12 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
+
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
