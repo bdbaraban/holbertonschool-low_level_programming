c8ac8814962f573a0883fa7ec2018a4767459533
Brennan D Baraban <375@holbertonschool.com>
2018-09-27 02:50:26 +0000

    Added null termination

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -20,7 +20,7 @@ char *_strcpy(char *dest, char *src)
 	while (src[index++])
 		len++;
 
-	for (index = 0; index <= len; index++)
+	for (index = 0; index <= len + 1; index++)
 		dest[index] = src[index];
 
 	return (dest);
