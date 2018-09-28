16627dc31b45a8482bee5a428ae5ff55ed1a88f9
Brennan D Baraban <375@holbertonschool.com>
2018-09-28 00:53:03 +0000

    Switched while loop to conditional

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -17,24 +17,23 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index] &&
-		    (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}'))
-			str[index] -= 32;
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			if (str[index - 1] == ' ' ||
+		            str[index - 1] == '\t' ||
+			    str[index - 1] == '\n' ||
+			    str[index - 1] == ',' ||
+			    str[index - 1] == ';' ||
+			    str[index - 1] == '.' ||
+			    str[index - 1] == '!' ||
+			    str[index - 1] == '?' ||
+			    str[index - 1] == '"' ||
+			    str[index - 1] == '(' ||
+			    str[index - 1] == ')' ||
+			    str[index - 1] == '{' ||
+			    str[index - 1] == '}')
+				str[index] -= 32;
+		}
 
 		index++;
 	}
