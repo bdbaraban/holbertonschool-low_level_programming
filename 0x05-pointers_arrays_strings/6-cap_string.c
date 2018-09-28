1c35559e9c285d57fa3b866ff18e580484ac9879
Brennan D Baraban <375@holbertonschool.com>
2018-09-28 00:39:04 +0000

    Fixed string indexing

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,8 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - 1] == ' ' ||
+		if (str[index] &&
+		    (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
@@ -32,7 +33,7 @@ char *cap_string(char *str)
 		    str[index - 1] == '(' ||
 		    str[index - 1] == ')' ||
 		    str[index - 1] == '{' ||
-		    str[index - 1] == '}')
+		    str[index - 1] == '}'))
 			str[index] -= 32;
 
 		index++;
