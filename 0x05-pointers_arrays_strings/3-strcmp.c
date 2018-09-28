64e8c6bce6d2966657d51edbdd7f8ec1bba06f85
Brennan D Baraban <375@holbertonschool.com>
2018-09-28 01:18:02 +0000

    Fixed iteration

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -16,8 +16,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1++ && *s2++ && *s1 == *s2)
-		;
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
 	if (*s1 == *s2)
 		return (0);
