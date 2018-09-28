290a0c23b62e8770a09a593d9aed8c698d569b88
Brennan D Baraban <375@holbertonschool.com>
2018-09-28 01:12:11 +0000

    Fixed return value

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,28 +10,18 @@
  * @s1: The first string to be compared.
  * @s2: The second string to be compared.
  *
- * Return: 15 if s1 > s2
+ * Return: Positive byte difference if s1 > s2
  *         0 if s1 = s2
- *         -15 if s1 < s2
+ *         Negative byte difference if s1 < s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int index = 0;
-	int s1_len = 0, s2_len = 0;
+	while (*s1++);
 
-	while (s1[index++])
-		s1_len++;
+	while (*s2++);
 
-	index = 0;
+	if (*s1 == *s2)
+		return (0);
 
-	while (s2[index++])
-		s2_len++;
-
-	if (s1_len > s2_len)
-		return (15);
-
-	else if (s1_len < s2_len)
-		return (-15);
-
-	return (0);
+	return (*s1 - *s2);
 }
