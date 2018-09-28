4fd98b3ac90ffb50fef07bf5293738144b7ad4cd
Brennan D Baraban <375@holbertonschool.com>
2018-09-28 05:38:12 +0000

    Completed bonus task 10

--- 0x05-pointers_arrays_strings/101-magic.c
+++ 0x05-pointers_arrays_strings/101-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  *(&n + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
