e7d9f3af410226da4456e5f9c59f25fdff13cbf9
Brennan D Baraban <375@holbertonschool.com>
2018-09-29 04:31:49 +0000

    Fixed styling

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		
+
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
