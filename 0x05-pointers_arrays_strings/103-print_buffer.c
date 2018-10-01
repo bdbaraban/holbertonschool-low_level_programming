d937c4c7d771c1c94eb836105dd738a4fb6976ad
Brennan D Baraban <375@holbertonschool.com>
2018-10-01 01:32:23 +0000

    Completed bonus task 12

---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,56 @@
+/*
+ * File: 103-print_buffer.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer 10 bytes at a time.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
+ */
+void print_buffer(char *b, int size)
+{
+	int byte, index;
+
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index += 2)
+		{
+			if ((index + byte) < size)
+			{
+				printf("%02x%02x ",
+				       *(b + index + byte),
+				       *(b + index + byte + 1));
+			}
+
+			else
+				printf("     ");
+		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) > size)
+				printf(" ");
+
+			else if (*(b + index + byte) >= 32 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+		printf("\n");
+}
