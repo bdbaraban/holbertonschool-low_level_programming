20a331fc61b3066520ea302802f22b286a15f879
Brennan D Baraban <375@holbertonschool.com>
2018-10-01 03:33:44 +0000

    Halted hex printing at size

--- 0x05-pointers_arrays_strings/103-print_buffer.c
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -19,23 +19,22 @@ void print_buffer(char *b, int size)
 	{
 		printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index += 2)
+		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-				printf("     ");
+				printf("  ");
 
 			else
-			{
-				printf("%02x%02x ",
-				       *(b + index + byte),
-				       *(b + index + byte + 1));
-			}
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
 		}
 
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-				printf(" ");
+				break;
 
 			else if (*(b + index + byte) >= 31 &&
 				 *(b + index + byte) <= 126)
