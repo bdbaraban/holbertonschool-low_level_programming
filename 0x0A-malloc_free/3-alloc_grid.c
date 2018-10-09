bfa99330f3ddc7ead397128f1148cdc51d925dfa
Brennan D Baraban <375@holbertonschool.com>
2018-10-09 21:04:38 +0000

    Completed tasks 3 and 4

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+/*
+ * File: 3-alloc_grid.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
+ */
+int **alloc_grid(int width, int height)
+{
+	int hgt_index, wid_index;
+	int **twoD;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+		return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD == NULL)
+			return (NULL);
+
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+
+	return (twoD);
+}
---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-free_grid.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2-dimensional array of integers.
+ * @grid: The 2-dimensional array of integers to be freed.
+ * @height: The height of grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+
+	free(grid);
+}
