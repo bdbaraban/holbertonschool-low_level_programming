1c0661a5369c81c9551f9e7e489894a8a3c11f48
Brennan D Baraban <375@holbertonschool.com>
2018-10-09 22:10:02 +0000

    Completed task 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,48 @@
+/*
+ * File: 5-argstostr.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a \n in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac = 0, av = NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, index, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 1; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 1; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	return (str);
+}
