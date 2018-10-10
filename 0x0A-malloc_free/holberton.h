e213655211c64453e604025ff4ff341624998340
Brennan D Baraban <375@holbertonschool.com>
2018-10-10 03:13:21 +0000

    Updated header file

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -5,5 +5,10 @@
  *       used in the 0x0A-malloc_free directory.
  */
 
-int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
