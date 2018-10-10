283d3b64d4ba4413adb86bfb58a750250d66d49d
Brennan D Baraban <375@holbertonschool.com>
2018-10-10 01:02:00 +0000

    Fixed error checking

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -57,10 +57,13 @@ char **strtow(char *str)
 	char **strings;
 	int index = 0, words, w, letters, l;
 
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL)
 		return (NULL);
 
 	words = count_words(str);
+
+	if (words == 0)
+		return (NULL);
 
 	strings = malloc(sizeof(char *) * (words + 1));
 
@@ -77,7 +80,14 @@ char **strtow(char *str)
 		strings[w] = malloc((sizeof(char) * letters) + 1);
 
 		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
 			return (NULL);
+
+		}
 
 		for (l = 0; l < letters; l++)
 			strings[w][l] = str[index++];
