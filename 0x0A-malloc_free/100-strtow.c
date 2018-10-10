71a600208278d3523aa70e84478627a2e2ffafa3
Brennan D Baraban <375@holbertonschool.com>
2018-10-10 00:34:40 +0000

    Completed bonus task 6

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,91 @@
+/*
+ * File: 100-strtow.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+int word_len(char *str)
+{
+	int index = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+		index++;
+
+	return (index);
+}
+
+/**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_words(char *str)
+{
+	int index = 0, words;
+
+	for (index = 0; *(str + index); index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+
+	return (words);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL)
+		return (NULL);
+
+	words = count_words(str);
+
+	strings = malloc(sizeof(char *) * words + 1);
+
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc((sizeof(char) * letters) + 1);
+
+		if (strings[w] == NULL)
+			return (NULL);
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
+	}
+
+	strings[w] = NULL;
+
+	return (strings);
+}
