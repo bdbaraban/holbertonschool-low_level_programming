0a1b81e665e479d721edd0df96b76f72610fcdf8
Brennan D Baraban <375@holbertonschool.com>
2018-10-11 23:33:03 +0000

    Removed whitespaces

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
  * @ptr: A pointer to the memory previously allocated.
  * @old_size: The size in bytes of the allocated space for ptr.
  * @new_size: The size in bytes for the new memory block.
- * 
+ *
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
  *         Otherwise - a pointer to the reallocated memory block.
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		mem = malloc(new_size);
-		
+
 		if (mem == NULL)
 			return (NULL);
 
