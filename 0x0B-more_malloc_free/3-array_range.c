124d1a2c301431dd3350fcc417f4c2d967eeeae6
Brennan D Baraban <375@holbertonschool.com>
2018-10-11 22:19:16 +0000

    Fixed array size calculcation

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -17,17 +17,19 @@
  */
 int *array_range(int min, int max)
 {
-	int *array, index;
+	int *array, index, size;
 
 	if (min > max)
 		return (NULL);
 
-	array = malloc(sizeof(int) * (max + 1));
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index <= max; index++)
+	for (index = 0; index < size; index++)
 		array[index] = min++;
 
 	return (array);
