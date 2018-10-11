20206163d92a96a3e9034703fcd02b7c0a27c5fb
Brennan D Baraban <375@holbertonschool.com>
2018-10-11 22:28:32 +0000

    Implemented memset function

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -7,6 +7,25 @@
 #include <stdlib.h>
 
 /**
+ * _memset - Fills a space in memory with an inputted char.
+ * @mem: - The space in memory.
+ * @c: The char to initialize mem with.
+ * @size: The size of mem.
+ *
+ * Return: A pointer to the filled space in memory.
+ */
+void *_memset(void *mem, char c, int size)
+{
+	char *filler = mem;
+	int index;
+
+	for (index = 0; index < size; index++)
+		filler[index] = c;
+
+	return (mem);
+}
+
+/**
  * _calloc - Allocates memory for an array of a certain number
  *           of elements each of an inputted byte size.
  * @nmemb: The number of elements.
@@ -27,5 +46,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 		return (NULL);
 
-	return (mem);
+	return (_memset(mem, '\0', size * nmemb));
 }
