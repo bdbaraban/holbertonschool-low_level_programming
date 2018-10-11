d10d36e4dbb3301a5813f6476ac50b966edecb8c
Brennan D Baraban <375@holbertonschool.com>
2018-10-11 23:31:45 +0000

    Fixed null checking

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -20,26 +20,41 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
 	char *ptr_copy, *filler;
-	unsigned int index = 0;
-
-	if (new_size == 0 && ptr != NULL)
-		return (NULL);
+	unsigned int index;
 
 	if (new_size == old_size)
 		return (ptr);
 
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+		
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr_copy = ptr;
 	mem = malloc(sizeof(*ptr_copy) * new_size);
 
 	if (mem == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	ptr_copy = ptr;
 	filler = mem;
 
-	while (*ptr_copy)
-		filler[index++] = *ptr_copy++;
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
 
 	free(ptr);
-
 	return (mem);
 }
