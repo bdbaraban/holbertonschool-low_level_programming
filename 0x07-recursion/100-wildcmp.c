27ec6f8be34476c2c450c821a8000b8584a4e115
Brennan D Baraban <375@holbertonschool.com>
2018-10-14 02:57:12 +0000

    Updated documentation

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -5,6 +5,11 @@
 
 #include "holberton.h"
 
+int strlen_no_wilds(char *str);
+char *iterate_wild(char *wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
 /**
  * strlen_no_wilds - Returns the length of a string,
  *                   ignoring wildcard characters.
--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -5,6 +5,10 @@
 
 #include "holberton.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
 /**
  * find_strlen - Returns the length of a string.
  * @s: The string to be measured.
