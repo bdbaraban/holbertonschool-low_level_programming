ea17414ba6e7d7f98cf76aef3d434c07dc40419c
Brennan D Baraban <375@holbertonschool.com>
2018-10-14 03:39:41 +0000

    Updated documentation

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -5,6 +5,9 @@
 
 #include "holberton.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
  * add_strings - Adds the numbers stored in two strings.
  * @n1: The string containing the first number to be added.
