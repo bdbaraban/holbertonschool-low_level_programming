d2dffaef9012ffb55caf193903b5c827751b91b2
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:05:43 +0000

    Implemented NULL checking

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -14,7 +14,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != '\0')
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
