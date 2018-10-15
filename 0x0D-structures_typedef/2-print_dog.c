27e1a1b7ad14106b6ec2c73161e2773a214cfd00
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:14:01 +0000

    Completed task 2

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+/*
+ * File: 2-print_dog.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ */
+void print_dog(struct dog *d)
+{ 	
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	
+	printf("Age: %lf\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
