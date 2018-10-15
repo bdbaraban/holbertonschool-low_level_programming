88c2f25dc02c860a1de0f60173f5b0822ea66657
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:49:15 +0000

    Fixed failure checking

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -4,6 +4,7 @@
  */
 
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * new_dog - Creates a new dog.
@@ -16,15 +17,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t doggo;
-	dog_t *me_doggo;
+	dog_t *me_doggo = doggo;
+
+	if (me_doggo == NULL)
+		return (NULL);
 
 	doggo.name = name;
 	doggo.age = age;
 	doggo.owner = owner;
-
-	me_doggo = &doggo;
-	if (me_doggo == '\0')
-		return ('\0');
 
 	return (me_doggo);
 }
