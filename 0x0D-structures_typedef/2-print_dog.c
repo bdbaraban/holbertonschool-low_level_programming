4dc70b2276fe8db6cfc8e8664e280a56186a941d
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:45:34 +0000

    Matched C90 standard

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -24,7 +24,7 @@ void print_dog(struct dog *d)
 	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %lf\n", d->age);
+		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
