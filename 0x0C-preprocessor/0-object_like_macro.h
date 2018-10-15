5aeaac004f83db95fe3a1405323c56db152f9c31
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 19:38:51 +0000

    Initialized directory + completed task 0

---
+++ 0x0C-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0C-preprocessor/README.md
@@ -0,0 +1,1 @@
+In this project, I learned about common predefined macros in C while practicing implementing my own and include-guarding header files.
