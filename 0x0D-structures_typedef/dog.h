b7005886c9d9bab3e0578c013a2e0657131462d0
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:50:11 +0000

    Updated header file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -20,14 +20,13 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
