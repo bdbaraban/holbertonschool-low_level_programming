427e9c27862a3262433a6824250f78eb1215df95
Brennan D Baraban <375@holbertonschool.com>
2018-10-15 21:25:14 +0000

    Implemented NULL age checking

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@
  * @d: The struct dog to be printed.
  */
 void print_dog(struct dog *d)
-{ 	
+{
 	if (d == NULL)
 		return;
 
@@ -19,8 +19,11 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	
-	printf("Age: %lf\n", d->age);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %lf\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
