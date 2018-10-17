0f5a06037f5c3566c3718eb44027938220201513
Brennan D Baraban <375@holbertonschool.com>
2018-10-17 18:42:35 +0000

    Initialized directory + completed task 0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
