bc6ab30dafbf6bbd2eabbf6cb79aeb800b7f0071
Brennan D Baraban <375@holbertonschool.com>
2018-10-17 22:25:49 +0000

    Condensed to single function

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -7,23 +7,6 @@
 #include <stdlib.h>
 
 /**
- * match_op - Matches an operator with a corresponding
- *            function located in an array of structs.
- * @ops: A pointer to an array of struct ops.
- * @op: A pointer to the operator to match.
- *
- * Return: The index of the struct within ops containing
- *         the function corresponding to op.
- */
-int match_op(op_t *ops, char *op)
-{
-	if (ops->op == NULL || *(ops->op) == *op)
-		return (0);
-
-	return (match_op(++ops, op) + 1);
-}
-
-/**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
  * @s: The operator passed as argument.
@@ -41,9 +24,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 	
-	i = match_op(ops, s);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
 	return (ops[i].f);
 }
