d59de9da0162daf8a768ae2da267e21dd5ecde33
Brennan D Baraban <375@holbertonschool.com>
2018-10-17 22:15:41 +0000

    Updated exit statuses

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -19,26 +19,27 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	int num1, num2;
 	char *op;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
 	num1 = atoi(argv[1]);
-	if (num1 == 0 && *(argv[1]) != 0)
-	{
-		printf("Error\n");
-		return (0);
-	}
-
 	op = argv[2];
-
 	num2 = atoi(argv[3]);
-	if (num2 == 0 && argv[3] != 0)
-	{
-		printf("Error\n");
-		return (0);
-	}
 
 	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
-		return (0);
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
