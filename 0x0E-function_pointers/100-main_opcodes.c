64c93ca1512fb189cfd1f0c43e59df3ee2c0becd
Brennan D Baraban <375@holbertonschool.com>
2018-10-17 23:43:55 +0000

    Completed bonus task 4

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+/*
+ * File: 100-main_opcodes.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, index;
+	int (*address)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		printf("%02x", ((char *)&address)[index]);
+
+		if (index == bytes - 1)
+			continue;
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
