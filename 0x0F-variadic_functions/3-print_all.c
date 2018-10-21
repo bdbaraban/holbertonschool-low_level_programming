5712e77128364077d32e5522db11dafd61df9105
bdbaraban <375@holbertonschool.com>
2018-10-21 00:39:07 +0000

    Condensed calls within nested while loops

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -85,6 +85,7 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, j = 0;
+	char *separator = "";
 	printer_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
@@ -101,15 +102,15 @@ void print_all(const char * const format, ...)
 		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
 			j++;
 
-		if (j < 4 && *(format + i + 1))
+		if (j < 4)
 		{
+			printf("%s", separator);
 			funcs[j].print(args);
-			printf(", ");
+			separator = ", ";
 		}
 
 		i++;
 	}
-	funcs[j].print(args);
 
 	printf("\n");
 
