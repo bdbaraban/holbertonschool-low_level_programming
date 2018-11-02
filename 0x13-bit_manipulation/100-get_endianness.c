c2ba7fdfc0a65aa61222cc0278e273cee01f226e
bdbaraban <375@holbertonschool.com>
2018-11-02 06:17:54 +0000

    Uploading file 100

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+/*
+ * File: 100-get_endianness.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if ((*endian + '0') == '1')
+		return (1);
+
+	return (0);
+}
