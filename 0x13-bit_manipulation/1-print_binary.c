b8a669e1e31d9cd7ce0d3107b497894c10df7fc1
bdbaraban <375@holbertonschool.com>
2018-11-03 01:07:53 +0000

    Compacted code - removed unnecessary conditional

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -11,12 +11,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	if ((n >> 1) > 0)
+	if (n > 1)
 		print_binary(n >> 1);
 
-	if ((n & 1) == 0)
-		_putchar('0');
-
-	else
-		_putchar('1');
+	_putchar((n & 1) + '0');
 }
