5426867bb7bb5da7ff7c591706926e8cfeded41e
bdbaraban <375@holbertonschool.com>
2018-11-06 23:36:54 +0000

    Implemented open call using O_APPEND for long files

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
@@ -67,6 +70,7 @@ int main(int argc, char *argv[])
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
@@ -77,7 +81,6 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
@@ -88,6 +91,8 @@ int main(int argc, char *argv[])
 		}
 
 		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (r > 0);
 
 	free(buffer);
