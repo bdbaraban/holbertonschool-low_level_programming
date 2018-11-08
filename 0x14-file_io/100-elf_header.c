570f508d78f996123bf9dba98a8432f09d7c6aab
bdbaraban <375@holbertonschool.com>
2018-11-08 06:29:17 +0000

    Accounted for big endian in print_type and print_entry

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -3,7 +3,6 @@
  * Auth: Brennan D Baraban
  */
 
-#define EV_CURRENT 2
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -19,7 +18,7 @@ void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type);
+void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
@@ -123,13 +122,13 @@ void print_data(unsigned char *e_ident)
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("  Version:                           %d ",
+	printf("  Version:                           %d",
 	       e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
-		printf("(current)\n");
+		printf(" (current)\n");
 		break;
 	default:
 		printf("\n");
@@ -195,9 +194,13 @@ void print_abi(unsigned char *e_ident)
 /**
  * print_type - Prints the type of an ELF header.
  * @e_type: The ELF type.
+ * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_type(unsigned int e_type)
+void print_type(unsigned int e_type, unsigned char *e_ident)
 {
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
+
 	printf("  Type:                              ");
 
 	switch (e_type)
@@ -209,7 +212,6 @@ void print_type(unsigned int e_type)
 		printf("REL (Relocatable file)\n");
 		break;
 	case ET_EXEC:
-	case 512:
 		printf("EXEC (Executable file)\n");
 		break;
 	case ET_DYN:
@@ -231,6 +233,13 @@ void print_type(unsigned int e_type)
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf("  Entry point address:               ");
+
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		e_entry = ((e_entry << 8) & 0xFF00FF00) |
+			  ((e_entry >> 8) & 0xFF00FF);
+		e_entry = (e_entry << 16) | (e_entry >> 16);
+	}
 
 	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)e_entry);
@@ -301,7 +310,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
 	print_abi(header->e_ident);
-	print_type(header->e_type);
+	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
