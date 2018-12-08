efaf42fbbe02e492d407f4b3c51607bbae5f8737
bdbaraban <375@holbertonschool.com>
2018-12-08 21:32:14 +0000

    Permitting insertion at end

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,15 +18,14 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
-	unsigned int i;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (i = 0; i < (idx - 1); i++)
+	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
-		if (tmp == NULL || tmp->next == NULL)
+		if (tmp == NULL)
 			return (NULL);
 	}
 
