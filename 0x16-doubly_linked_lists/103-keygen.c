2c912a5c6ab6a89dfb490257c8e4a3f84e0044af
bdbaraban <375@holbertonschool.com>
2018-12-10 22:42:33 +0000

    Fixing -Werrors

--- 0x16-doubly_linked_lists/103-keygen.c
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -16,25 +16,26 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex, i, tmp;
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (strlen(argv[1]) ^ 59) & 63;
+	tmp = (len ^ 59) & 63;
 	password[0] = codex[tmp];
 
 	tmp = 0;
-	for (i = 0; i < strlen(argv[1]); i++)
+	for (i = 0; i < len; i++)
 		tmp += argv[1][i];
 	password[1] = codex[(tmp ^ 79) & 63];
 
 	tmp = 1;
-	for (i = 0; i < strlen(argv[1]); i++)
+	for (i = 0; i < len; i++)
 		tmp *= argv[1][i];
 	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
-	for (i = 0; i < strlen(argv[1]); i++)
+	for (i = 0; i < len; i++)
 	{
 		if (argv[1][i] > tmp)
 			tmp = argv[1][i];
@@ -43,7 +44,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	password[3] = codex[rand() & 63];
 
 	tmp = 0;
-	for (i = 0; i < strlen(argv[1]); i++)
+	for (i = 0; i < len; i++)
 		tmp += (argv[1][i] * argv[1][i]);
 	password[4] = codex[(tmp ^ 239) & 63];
 
