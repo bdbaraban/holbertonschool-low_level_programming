4589d6a705fd877101f250bdfb9d758f288bbe35
bdbaraban <375@holbertonschool.com>
2019-01-15 19:21:46 +0000

    Edit to not print anything for NULL hash tables

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -19,10 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
-	{
-		printf("{}\n");
 		return;
-	}
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
