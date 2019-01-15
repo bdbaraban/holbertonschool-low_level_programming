aa79d48d367b262d9070b5b240ffcaa1c303f038
bdbaraban <375@holbertonschool.com>
2019-01-15 19:13:18 +0000

    Implement key/value updates for pre-existing keys

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to a hash table.
+ * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
@@ -17,30 +17,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	unsigned long int index;
+	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
 	new->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
 		return (0);
 	}
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
-
-	index = key_index((const unsigned char *)key, ht->size);
+	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
