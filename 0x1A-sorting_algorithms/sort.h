232228b1c4a4a25f4f58fec7702f839934630244
bdbaraban <375@holbertonschool.com>
2019-02-05 02:19:49 +0000

    Delete un-implemented prototype

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -32,6 +32,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
