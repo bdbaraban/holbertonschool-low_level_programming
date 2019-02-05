6dd261d31e87d76f99a8dc66e4ec2de8a9d38c28
bdbaraban <375@holbertonschool.com>
2019-02-05 01:00:49 +0000

    Remove trailing whitespace in documentation

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -24,7 +24,7 @@ void swap(int *a, int *b)
  * @array: An array of integers to sort.
  * @size: The size of the array.
  *
- * Description: Prints the array after each swap. 
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
