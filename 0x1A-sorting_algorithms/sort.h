b19f675bdc243bc4b994af77949b9554c639dadd
bdbaraban <375@holbertonschool.com>
2019-02-05 02:17:46 +0000

    Remove inclusion of stdio.h

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,7 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -33,6 +32,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-/*void sort_deck(deck_node_t **deck); */
+void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
