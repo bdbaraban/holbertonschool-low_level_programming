6cb74680459f85517c6010ad7f70aa75631c6fb6
bdbaraban <375@holbertonschool.com>
2019-02-07 22:13:34 +0000

    Improve readability by using inner while instead of for loop for swapping

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -6,11 +6,11 @@
 #include "sort.h"
 
 /**
- * swap - Swap two integers in an array.
+ * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
@@ -35,15 +35,18 @@ void shell_sort(int *array, size_t size)
 		return;
 
 	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
+		gap *= 3 + 1;
 
 	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
 			j = i;
-			for (; j >= gap && array[j - gap] > array[j]; j -= gap)
-				swap(array + j, array + (j - gap));
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
 		}
 		print_array(array, size);
 	}
