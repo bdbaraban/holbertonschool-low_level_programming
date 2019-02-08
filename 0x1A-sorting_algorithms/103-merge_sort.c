d253fe7a3ed459b0392441372b77cedb34b30474
bdbaraban <375@holbertonschool.com>
2019-02-08 01:17:45 +0000

    Simplify subarray printing calls and rename merge function to merge_subarr

--- 0x1A-sorting_algorithms/103-merge_sort.c
+++ 0x1A-sorting_algorithms/103-merge_sort.c
@@ -5,34 +5,29 @@
 
 #include "sort.h"
 
-void merge(int *subarray, int *buff, size_t front, size_t mid, size_t back);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 
 /**
- * merge - Sort and merge a subarray of integers.
- * @subarr: A subarry of an array of integers to sort.
+ * merge_subarr - Sort a subarray of integers.
+ * @subarr: A subarray of an array of integers to sort.
  * @buff: A buffer to store the sorted subarray.
  * @front: The front index of the array.
  * @mid: The middle index of the array.
  * @back: The back index of the array.
  */
-void merge(int *subarr, int *buff, size_t front, size_t mid, size_t back)
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
 {
 	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
-	for (i = front; i < mid; i++)
-	{
-		printf("%d", subarr[i]);
-		(i != mid - 1) ? printf(", ") : printf("\n");
-	}
+	print_array(subarr, mid - front);
+
 	printf("[right]: ");
-	for (j = mid; j < back; j++)
-	{
-		printf("%d", subarr[j]);
-		(j != back - 1) ? printf(", ") : printf("\n");
-	}
+	print_array(subarr, back - mid);
 
 	for (i = front, j = mid; i < mid && j < back; k++)
 		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
@@ -44,11 +39,7 @@ void merge(int *subarr, int *buff, size_t front, size_t mid, size_t back)
 		subarr[i] = buff[k++];
 
 	printf("[Done]: ");
-	for (i = front; i < back; i++)
-	{
-		printf("%d", subarr[i]);
-		(i != back - 1) ? printf(", ") : printf("\n");
-	}
+	print_array(subarr, back - front);
 }
 
 /**
@@ -67,7 +58,7 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
 		mid = front + (back - front) / 2;
 		merge_sort_recursive(subarr, buff, front, mid);
 		merge_sort_recursive(subarr, buff, mid, back);
-		merge(subarr, buff, front, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
 	}
 }
 
