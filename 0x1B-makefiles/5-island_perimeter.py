6c54b057134b539791dc875d9bdd704d39204ba1
bdbaraban <375@holbertonschool.com>
2019-02-11 23:37:37 +0000

    Remove type checking

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -12,11 +12,6 @@ def island_perimeter(grid):
     Returns:
         The perimeter of the island defined in grid.
     """
-    if (type(grid) != list or
-            not all(type(row) == list for row in grid) or
-            not all(type(n) == int for n in [e for r in grid for e in r])):
-        return 0
-
     width = len(grid[0])
     height = len(grid)
     perimeter = 0
