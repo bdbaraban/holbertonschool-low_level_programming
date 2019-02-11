083c877397846df787b23827d98f1f4a953d71b3
bdbaraban <375@holbertonschool.com>
2019-02-11 23:47:23 +0000

    Refactor using island size/internal edge counting

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -14,17 +14,15 @@ def island_perimeter(grid):
     """
     width = len(grid[0])
     height = len(grid)
-    perimeter = 0
+    edges = 0
+    size = 0
 
     for i in range(height):
         for j in range(width):
-            if grid[i][j] == 0:
+            if grid[i][j] == 1:
+                size += 1
                 if (j > 0 and grid[i][j - 1] == 1):
-                    perimeter += 1
+                    edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
-                    perimeter += 1
-                if (j < width - 1 and grid[i][j + 1] == 1):
-                    perimeter += 1
-                if (i < height - 1 and grid[i + 1][j] == 1):
-                    perimeter += 1
-    return perimeter
+                    edges += 1
+    return size * 4 - edges * 2
