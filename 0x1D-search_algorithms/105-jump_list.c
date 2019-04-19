b652eedf2fc8596816f39b8d3e39b43534f00c24
bdbaraban <375@holbertonschool.com>
2019-04-19 00:14:54 +0000

    Upload task 105: Jump search with singly linked lists

---
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -0,0 +1,63 @@
+/*
+ * File: 105-jump_list.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
+ * @size: The number of nodes in the list.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size, s;
+	listint_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	step_size = sqrt(size);
+	for (step = step_size, jump = list; jump->index < step;)
+	{
+		if (jump->index + 1 >= size)
+			break;
+		jump = jump->next;
+	}
+
+	for (node = list; node->n < value;)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+		if (jump->index + 1 >= size || jump->n > value)
+			break;
+		node = jump;
+		for (s = step, step += step_size; s < step; s++)
+		{
+			if (jump->index + 1 >= size)
+				break;
+			jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->n < value; node = node->next)
+	{
+		if (node->index + 1 >= size)
+			break;
+		printf("Value checked index [%ld] = [%d]\n", node->index, node->n);
+	}
+	printf("Value checked index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
