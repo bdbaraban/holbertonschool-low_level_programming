c778a45dc228d1146164bd96058ec7291f88de88
bdbaraban <375@holbertonschool.com>
2019-04-20 00:22:23 +0000

    Replicate successful task 100 fix: set first for loop to iterate jump and second to iterate index

--- 0x1D-search_algorithms/105-jump_list.c
+++ 0x1D-search_algorithms/105-jump_list.c
@@ -20,42 +20,30 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step, step_size, s;
+	size_t step, step_size;
 	listint_t *node, *jump;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	for (step = step_size = sqrt(size), jump = list; jump->index < step;)
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		if (jump->index + 1 >= size)
-			break;
-		jump = jump->next;
-	}
-
-	for (node = list; node->n < value;)
-	{
-		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
-		if (jump->index + 1 >= size || jump->n > value)
-			break;
 		node = jump;
-		for (s = step, step += step_size; s < step; s++)
+		for (step += step_size; jump->index < step; jump = jump->next)
 		{
-			if (jump->index + 1 >= size)
+			if (jump->index + 1 == size)
 				break;
-			jump = jump->next;
 		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n",
 			node->index, jump->index);
 
-	for (; node->n < value; node = node->next)
-	{
-		if (node->index + 1 >= size)
-			break;
+	for (; node->index < jump->index && node->n < value; node = node->next)
 		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-	}
 	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
 	return (node->n == value ? node : NULL);
