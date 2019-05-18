1694d8284659eaef7525ec8c068e5b0d219ecdbd
bdbaraban <375@holbertonschool.com>
2019-05-18 05:25:37 +0000

    Upload folder of test files

---
+++ 0x17-dynamic_libraries/tests/0-main.c
@@ -0,0 +1,13 @@
+#include "../holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
