4afb4da6ca64d28950e42159f0b594c2a2bf2954
bdbaraban <375@holbertonschool.com>
2019-05-18 05:18:09 +0000

    Upload folder of test files

---
+++ 0x14-file_io/tests/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
+}
