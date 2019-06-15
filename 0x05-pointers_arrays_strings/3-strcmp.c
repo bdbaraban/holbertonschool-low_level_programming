770df42ba208e87f110d54223fbdf203e8a374d9
bdbaraban <375@holbertonschool.com>
2019-06-15 21:19:14 +0000

    Always return byte difference and improve specifity of documentation

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -6,13 +6,13 @@
 #include "holberton.h"
 
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
  *
- * Return: Positive byte difference if s1 > s2
- *         0 if s1 = s2
- *         Negative byte difference if s1 < s2
+ * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference of the first unmatched characters.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -22,8 +22,5 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	if (*s1 != *s2)
-		return (*s1 - *s2);
-
-	return (0);
+	return (*s1 - *s2);
 }
