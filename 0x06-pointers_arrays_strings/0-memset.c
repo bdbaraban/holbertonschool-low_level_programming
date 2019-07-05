ae82aacef8d31d50debeb1356ddb6e5cba2cef5d
bdbaraban <375@holbertonschool.com>
2019-07-04 17:55:48 -0700

    Update function documentation to match that used in Holberton blog article

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -6,20 +6,20 @@
 #include "holberton.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area
- *           pointed to by s with the constant byte b.
- * @s: The memory area to be filled.
- * @b: The character to fill the memory area.
+ * memset - Fills the first n bytes of the memory area
+ *          pointed to by @s with the constant byte @c.
+ * @s: A pointer to the memory area to be filled.
+ * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
- * Return: A pointer to the filled memory area s.
+ * Return: A pointer to the filled memory area @s.
  */
-char *_memset(char *s, char b, unsigned int n)
+void *memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 
 	for (index = 0; index < n; index++)
-		s[index] = b;
+		s[index] = c;
 
 	return (s);
 }
