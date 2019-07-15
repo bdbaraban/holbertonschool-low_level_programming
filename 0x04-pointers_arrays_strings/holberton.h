b2bbe0ae1219a38c17d07f033ac9c07496e15f12
bdbaraban <375@holbertonschool.com>
2019-07-14 21:30:18 -0700

    Align strlen prototype with updates

--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -8,10 +8,12 @@
  *       used in the 0x04-pointers_arrays_strings directory.
  */
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
