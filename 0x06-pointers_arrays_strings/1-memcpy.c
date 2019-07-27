83a29e2538fbb76a68299839e7969a07154d97f6
bdbaraban <375@holbertonschool.com>
2019-07-26 19:23:04 -0700

    Refactor function to align with real function prototype

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -6,19 +6,22 @@
 #include "holberton.h"
 
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
- * @dest: The memory area to be copied to.
- * @src: The memory area to be copied from.
- * @n: The number of bytes to be copied.
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
  *
- * Return: A pointer to dest.
+ * Return: A pointer to the destination buffer @dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void *_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
-		dest[index] = src[index];
+		destination[index] = source[index];
 
 	return (dest);
 }
